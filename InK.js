3fbb4695ef75dab1e82a120cb8f3ad593bdd46cd
Nader <nukleas2001@gmail.com>
2014-04-08 14:22:41 -0400

    linting, cleaning up some vars

--- InK.js
+++ InK.js
@@ -1,4 +1,6 @@
-﻿/*
+﻿/*jslint regexp:true */
+/*global app*/
+/*
     InK: InCopy/K4 automation suite
     By Nader Heidari
 */
@@ -6,8 +8,8 @@
     The following junk is a pseudo-testing thing for the parser
     in case I am not able to access InCopy.
 */
-if (typeof app === "undefined") {
-    app = {
+if (app === undefined) {
+    var app = {
         activeDocument: {
             stories: [{
                 notes: [{
@@ -38,20 +40,22 @@ var InK = {
         }
     },
     interp: {
-        getAllNotes: function() {
+        getAllNotes: function () {
+		    "use strict";
             var i, results = [],
                 notes,
                 noteQueue = [],
-                item;
-            for (i = 0; i < app.activeDocument.stories.length; i++) {
-                notes = app.activeDocument.stories[i].notes;
-                noteQueue = (function(e) {
+                item,
+				inCopyObjToArray = function (e) {
                     var i, arr = [];
-                    for (i = 0; i < notes.length; i++) {
-                        arr.push(notes[i])
+                    for (i = 0; i < e.length; i += 1) {
+                        arr.push(e[i]);
                     }
                     return arr;
-                })(notes);
+                };
+            for (i = 0; i < app.activeDocument.stories.length; i += 1) {
+                notes = app.activeDocument.stories[i].notes;
+                noteQueue = inCopyObjToArray(notes);
                 item = noteQueue.shift();
                 while (item !== undefined) {
                     results.push(item);
@@ -60,10 +64,12 @@ var InK = {
             }
             return results;
         },
-        extractCommands: function(notes) {
+        extractCommands: function (notes) {
+		    "use strict";
             var commands = [],
-                commandRegex = /\{.*?\}/;
-            for (var i = 0; i < notes.length; i++) {
+                commandRegex = /\{.*?\}/,
+				i;
+            for (i = 0; i < notes.length; i += 1) {
                 if (commandRegex.test(notes[i].texts[0].contents)) {
                     commands.push({
                         note: notes[i],
@@ -73,20 +79,23 @@ var InK = {
             }
             return commands;
         },
-        parseCommands: function() {
-            var derp = this.getAllNotes();
-            var herp = this.extractCommands(derp);
-            for (var i = 0; i < herp.length; i++) {
+        parseCommands: function () {
+		    "use strict";
+		    var derp, herp, i;
+            derp = this.getAllNotes();
+            herp = this.extractCommands(derp);
+            for (i = 0; i < herp.length; i += 1) {
                 herp[i].command = this.parseCommand(herp[i].contents);
             }
             return herp;
         },
-        parseCommand: function(string) {
-            var i, commandArray;
-            var command = string.match(/\{(.*?)\}/)[1];
+        parseCommand: function (string) {
+		    "use strict";
+            var i, commandArray, command;
+            command = string.match(/\{(.*?)\}/)[1];
             commandArray = command.replace(/^\s+|\s+$/g, "").replace(/(^\{|\}$)/g, "").split(":");
             if (commandArray.length > 1) {
-                for (i = 0; i < commandArray.length; i++) {
+                for (i = 0; i < commandArray.length; i += 1) {
                     commandArray[i] = commandArray[i].replace(/^\s+|\s+$/g, "");
                 }
                 return commandArray;
@@ -97,23 +106,25 @@ var InK = {
 InK.interp.parseCommands();
 
 function getMetadataMap() {
-    var defs = app.k4Publications[0].k4MetaDataDefs;
-    var obj = {};
-    for (var i = 0; i < defs.length; i++) {
+    "use strict";
+    var defs, obj, i, j;
+    defs = app.k4Publications[0].k4MetaDataDefs;
+    obj = {};
+    for (i = 0; i < defs.length; i += 1) {
 
         switch (defs[i].k4MetaDataDataType.toString()) {
-            case "K4_STRING":
-                obj[defs[i].k4Name] = "string";
-                break;
-            case "K4_VALUE_LIST":
-                obj[defs[i].k4Name] = [];
-                for (var j = 0; j < defs[i].k4MetaDataDefValueLists.length; j++) {
-                    obj[defs[i].k4Name].push(defs[i].k4MetaDataDefValueLists[j].k4Name);
-                }
-                break;
-            case "K4_INTEGER":
-                obj[defs[i].k4Name] = [true, false];
-                break;
+        case "K4_STRING":
+            obj[defs[i].k4Name] = "string";
+            break;
+        case "K4_VALUE_LIST":
+            obj[defs[i].k4Name] = [];
+            for (j = 0; j < defs[i].k4MetaDataDefValueLists.length; j += 1) {
+				obj[defs[i].k4Name].push(defs[i].k4MetaDataDefValueLists[j].k4Name);
+            }
+            break;
+        case "K4_INTEGER":
+            obj[defs[i].k4Name] = [true, false];
+            break;
         }
     }
     return obj;
